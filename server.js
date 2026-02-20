1a617565a368c4fb249191c9e1eb9f993b736c31
abbaskhanafer2012 <abbaskhanafer2012@gmail.com>
2026-02-20 09:09:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ app.get("/", (req, res) => {
 // ⭐⭐ هذا أهم جزء ⭐⭐
 // Meta سيستدعيه عند Verify
 app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
   if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("Webhook verified!");
-    return res.status(200).send(challenge); // لازم يرجع الرقم فقط
+    console.log("WEBHOOK VERIFIED");
+    res.status(200).send(challenge); // أهم سطر
   } else {
-    return res.sendStatus(403);
+    res.sendStatus(403);
   }
 });
-
 
 // استقبال الرسائل
 app.post("/webhook", (req, res) => {
